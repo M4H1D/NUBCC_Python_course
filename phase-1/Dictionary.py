77a5674b6ae5a678d76ccca7374e4eacd22be18e
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2021-04-12 00:44:35 +0600

    Add files via upload

---
+++ phase-1/Dictionary.py
@@ -0,0 +1,9 @@
+#write a short CV about me
+print("                      C͢u͢r͢r͢i͢c͢u͢l͢u͢m͢ V͢i͢t͢a͢e͢                           ")
+cv={
+	"Name":"Mahidul Islam",
+	"Current study":"HSC pass",
+	"Programming skill":["C","Python","HTML"],
+	"Hobbies":["Travelling"],
+	"Career":"Student"}
+print(cv)
