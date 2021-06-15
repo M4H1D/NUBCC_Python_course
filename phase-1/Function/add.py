2aa406fbc197847fb05e13fb1d464c6a5692c6d4
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2021-06-15 15:54:22 +0600

    Create add.py

---
+++ phase-1/Function/add.py
@@ -0,0 +1,9 @@
+#add two number
+
+def add(num1,num2):
+
+	print("add = ",num1+num2)num1=int(input("please input a integer number = "))
+
+num2=int(input("please input a another integer number = "))
+
+add(num1,num2)
