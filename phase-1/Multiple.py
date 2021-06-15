ec431cc4f2d87c562b6bdc20350e50d97c3b1403
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2021-06-15 16:15:29 +0600

    Add files via upload

---
+++ phase-1/Multiple.py
@@ -0,0 +1,11 @@
+#multiple
+#multiple
+def multiple(num1,num2):
+	print("multiple =",num1*num2)
+num1=int(input("please enter numer_1 = "))
+num2=int(input("please enter number_2 = "))
+multiple(num1,num2)	
+
+
+
+	
---
+++ phase-1/substraction.py
@@ -0,0 +1,8 @@
+def substraction(num1,num2):
+	if num1>num2:
+		print("substraction = ",num1-num2)
+	else:
+		print("substraction = ",num2-num1)
+num1=int(input("input a integer number = "))
+num2=int(input("input another number "))
+substraction(num1,num2)
